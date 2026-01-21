fa96a29c8bc98b4770c53c75cb94bb8d4011958b
Priyanshu Choudhary <priyanshu@Priyanshus-MacBook-Air-2.local>
2026-01-21 21:18:06 +0530

    added logout fn

--- Experiment 2/experiment2/src/components/Header.jsx
+++ Experiment 2/experiment2/src/components/Header.jsx
@@ -1,17 +1,25 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import {useAuth} from "../context/AuthContext"
 
 const Header=()=>{
+    const {isAuthenticated,setIsAuthenticated}=useAuth();
+    const navigate=useNavigate();
+
+    const handleLogout=()=>{
+        setIsAuthenticated(false);
+        navigate("/login");
+    }
+
     return (
         <>
-        <h2>EcoTrack</h2>
-        <nav>
-            <Link to="/">Dashboard</Link>
-            <Link to="/logs">Logs</Link>
-            <Link to="/login">Login</Link>
-            <Link to="/support">Support</Link>
+        <h2 style={{fontWeight:"bolder",backgroundColor:"pink",padding:10}}>EcoTrack</h2>
+        <nav style={{display:"flex", gap:5}} >
+            <Link style={{color:"black"}} to="/">Dashboard</Link>
+            <Link style={{color:"black"}} to="/logs">Logs</Link>
+            <Link style={{color:"black"}} to="/support">Support</Link>
+            {isAuthenticated?(<button style={{color:"black",cursor:"pointer"}} onClick={handleLogout}>Logout</button>):(<Link style={{color:"black"}} to="/login">Login</Link>)}
         </nav>
         </>
     )
 };
-
 export default Header;
