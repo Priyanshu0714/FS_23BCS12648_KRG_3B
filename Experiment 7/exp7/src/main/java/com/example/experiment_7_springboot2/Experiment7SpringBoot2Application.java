885b51ca9bb2a593db3c791079eb8943337f1876
Priyanshu Choudhary <choudhary.priyanshu1401@gmail.com>
2026-03-16 17:56:11 +0530

    added experiment7

---
+++ Experiment 7/exp7/src/main/java/com/example/experiment_7_springboot2/Experiment7SpringBoot2Application.java
@@ -0,0 +1,15 @@
+package com.example.experiment_7_springboot2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Experiment7SpringBoot2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Experiment7SpringBoot2Application.class, args);
+    }
+
+}
+
+// Priyanshu Choudhary_23bcs12648
